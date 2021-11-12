6b319c4869be137d38a479a6a984dc14ad837320
Danilo <magliadanilo@gmail.com>
2021-11-12 11:06:16 +0100

    Cambiate ombre della card

--- wudweb/src/wud/Card.tsx
+++ wudweb/src/wud/Card.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export class Card extends React.Component<{ width: string, color: string}> {
     render() {
         return (
-            <div className={`col-span-${this.props.width} bg-${this.props.color} py-4 px-8 shadow-lg rounded-lg my-20`}>{this.props.children}</div>
+            <div className={`col-span-${this.props.width} bg-${this.props.color} 
+                            py-4 px-8 shadow-xl rounded-xl my-20`}>{this.props.children}</div>
         );
     }
 }
+
