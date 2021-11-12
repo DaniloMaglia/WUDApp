1069dc54d8017b2845d7f79c2adce388b7c13b06
Giacomo <biagioni11@gmail.com>
2021-11-12 20:21:53 +0100

    Creato componente ContactList e testata una ui

--- wudweb/src/wud/Card.tsx
+++ wudweb/src/wud/Card.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export class Card extends React.Component<{ width: string, color: string}> {
     render() {
         return (
-            <div className={`col-span-${this.props.width} bg-${this.props.color} 
-                            py-4 px-8 shadow-xl rounded-xl my-20`}>{this.props.children}</div>
+            <div className={`col-span-${this.props.width} bg-${this.props.color} shadow-xl rounded-xl my-20`}>{this.props.children}</div> // py-4 px-8
         );
     }
 }
