41a5e0a1d2134bc00fd69b408cfff20627256c5a
Danilo <magliadanilo@gmail.com>
2021-11-13 23:10:16 +0100

    Piccoli cambiamenti

--- wudweb/src/wud/chatlist/ChatList.tsx
+++ wudweb/src/wud/chatlist/ChatList.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { ChatListElement } from '..';
 
 export class ChatList extends React.Component {
-    private activeChatList!: JSX.Element[];
+    private activeChatList: JSX.Element[] = [];
 
     public gatherActiveChats() { // TODO: Fare in modo che li prenda da firebase (Manca la richiesta nell'API)
-        this.activeChatList = [];
-        this.activeChatList.push(<ChatListElement name="maglia danilo" />);
-        this.activeChatList.push(<ChatListElement name="petru marcel marincas" />);
+        //this.activeChatList = [];
+        this.activeChatList = [
+            <ChatListElement name="maglia danilo" />,
+            <ChatListElement name="biagioni giacomo" />,
+            <ChatListElement name="petru marincas marcel" />,
+            <ChatListElement name="mattia ordonselli" />,
+            <ChatListElement name="sabbatini nicola" />,
+        ]
 
         // FIXME: "Warning: Each child in a list should have a unique "key" prop."
     }
@@ -16,8 +21,8 @@ export class ChatList extends React.Component {
         this.gatherActiveChats(); // TODO: Viene chiamato ogni volta che renderizza nuovamente la pagina (Capire se è ottimizzato)
         return (
             <div className={''}>
-                <p className={'shadow-lg rounded-b-2xl text-4xl font-semibold text-center text-indigo-500 uppercase py-2'}>chat</p>
-                <div className={'flex flex-col items-center divide-y overflow-y-hidden overflow-x-hidden text-center'}>
+                <span className={'items-center justify-center h-20 shadow-lg w-full rounded-2xl text-4xl font-semibold text-center text-indigo-500 uppercase py-2'}>contacts</span>
+                <div className={'items-center overflow-y-hidden overflow-x-hidden text-center'}>
                     {this.activeChatList}
                 </div>
             </div>
--- wudweb/src/wud/chatlist/ChatListElement.tsx
+++ wudweb/src/wud/chatlist/ChatListElement.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export class ChatListElement extends React.Component<{name : string}> {
     render() {        
         return (
-            <div className={'w-full py-4 shadow-sm hover:shadow-md cursor-pointer'}>{this.props.name}</div>
+            <div className={'w-full py-4 rounded-xl hover:shadow-ar transform cursor-pointer'}>{this.props.name}</div>
         );
     }
 }
