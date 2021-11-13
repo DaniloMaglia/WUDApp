0655e0e400b1e01ad90bb1869a1278f56dbcabdc
Danilo <magliadanilo@gmail.com>
2021-11-13 19:02:26 +0100

    Finito http driver

--- wudweb/src/wud/utils/HTTPDriver.ts
+++ wudweb/src/wud/utils/HTTPDriver.ts
@@ -1,19 +1,61 @@
-import internal from "stream";
+import axios, {AxiosResponse, AxiosRequestHeaders} from "axios";
+class RequestArgument {
+    key: string;
+    value: any;
 
-const axios = require("axios");
+    constructor(key: string, value: any) {
+        this.key = key;
+        this.value = value!;
+    }
 
+    public toString(){
+        if (typeof this.value == "string") {
+            this.value.replace(' ', '+');
+        }
+
+        return `${this.key}=${this.value}`;
+    }
+    
+    public static toObject(args: Array<RequestArgument>) : Object {
+        let argsObj: { [key: string] : any } = {};
+
+        args.forEach((arg) => {
+            argsObj[arg.key] = arg.value;
+        });
+
+        return argsObj;
+    }
+}
 class HTTPDriver {
     url: string;
     port: number;
 
-    constructor(url: string, port: number){
+    constructor(url: string, port: number) {
         this.url = url;
         this.port = port;
     }
 
-    static post(url: string, port: number, body: Object, headers: Object){
-        return axios.post(`${url}:${port}`, body, {headers});
+    public post(path: string, body: Array<RequestArgument>, headers: AxiosRequestHeaders = {}): Promise<AxiosResponse> {
+        return axios.post(`http://${this.url}:${this.port}/${path}`, RequestArgument.toObject(body), {headers});
+    }
+
+    public get(path: string, args: Array<RequestArgument>, headers: AxiosRequestHeaders = {}): Promise<AxiosResponse> {
+        let argsString = this.parseArguments(args);
+        return axios.get(`http://${this.url}:${this.port}/${path}?${argsString}`, headers);
+    }
+
+    private parseArguments(args: Array<RequestArgument>) : string {
+        let argsString: string = '';
+        args.forEach((arg : RequestArgument, idx : number) => {
+            argsString += arg.toString();
+            if (idx < ( args.length - 1)) {
+                argsString += '&';
+            }
+        });
+
+        console.log(argsString);
+        return argsString;
     }
 }
 
-export {HTTPDriver};
+export {HTTPDriver, RequestArgument};
